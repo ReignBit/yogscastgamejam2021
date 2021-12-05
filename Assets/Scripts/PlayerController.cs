379c10eb5b19b9c9e863750ab621b124bf24bdb1
ReignBit <bitbytegaming@gmail.com>
2021-12-05 16:21:02 +0000

    Moved tilemap to TilemapManager

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,8 +5,6 @@ using UnityEngine.Tilemaps;
 
 public class PlayerController : MonoBehaviour
 {
-	[SerializeField] private Tilemap groundMap;
-	[SerializeField] private Tilemap collisionsMap;
 	[SerializeField] private float moveSpeed;
 	private Camera camera;
 
@@ -14,18 +12,18 @@ public class PlayerController : MonoBehaviour
 	private PlayerMovement controls;
 	private Vector3 destination;
 
-	private void Awake() {
+	void Awake() {
 		mouseInput = new MouseInput();
 		controls = new PlayerMovement();
 		camera = Camera.main;
 	}
 
-	private void OnEnable() {
+	void OnEnable() {
 		mouseInput.Enable();
 		controls.Enable();
 	}
 
-	private void OnDisable() {
+	void OnDisable() {
 		mouseInput.Disable();
 		controls.Disable();
 	}
@@ -39,30 +37,36 @@ public class PlayerController : MonoBehaviour
 
     }
 
-	private void MouseClick() {
+	void MouseClick() {
 		Vector2 mousePosition = mouseInput.Mouse.MousePosition.ReadValue<Vector2>();
 		mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-		Vector3Int gridPosition = groundMap.WorldToCell(mousePosition);
-		if (groundMap.HasTile(gridPosition))
+		Vector3Int gridPosition = TilemapManager.instance.Ground.WorldToCell(mousePosition);
+		
+        if (TilemapManager.instance.Ground.HasTile(gridPosition))
 			destination = mousePosition;
+            RoundManager.instance.EndPlayerTurn();
 	}
 
-	private void Move(Vector2 direction) {
+    void Move(Vector2 direction) {
 		Debug.Log(transform.position + (Vector3)direction);
 		if (CanMove(direction))
-			transform.position += (Vector3)direction;
+        {
+		    transform.position += (Vector3)direction;
+        }
 	}
 
-	private bool CanMove(Vector2 direction) {
+	bool CanMove(Vector2 direction) {
 		Vector2 position = camera.ScreenToWorldPoint(direction);
-		Vector3Int gridPosition = groundMap.WorldToCell(position + direction);
-		return groundMap.HasTile(gridPosition) && !collisionsMap.HasTile(gridPosition);
+		Vector3Int gridPosition = TilemapManager.instance.Ground.WorldToCell(position + direction);
+		return TilemapManager.instance.Ground.HasTile(gridPosition) && !TilemapManager.instance.Collision.HasTile(gridPosition);
 	}
 
     // Update is called once per frame
     void Update()
     {
 		if (Vector3.Distance(transform.position, destination) > 0.1f)
-			transform.position = Vector3.MoveTowards(transform.position, destination, moveSpeed * Time.deltaTime);
+		{
+            transform.position = Vector3.MoveTowards(transform.position, destination, moveSpeed * Time.deltaTime);
+        }
     }
 }
