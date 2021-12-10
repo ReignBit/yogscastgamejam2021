fb580e6b5972096d2ed8e5deb88769eab3e95cfb
Fontacorn <Fontacorn@gmail.com>
2021-12-10 11:39:02 +0200

    Presents' appearance are chosen randomly

--- Assets/Scripts/Present.cs
+++ Assets/Scripts/Present.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Present : MonoBehaviour
 {
-    // Start is called before the first frame update
+	[SerializeField] private Sprite[] presentSprites;
+
     void Start()
     {
+		GetComponent<SpriteRenderer>().sprite = presentSprites[Random.Range(0, presentSprites.Length)];
         RoundManager.instance.AddPresent(this);
         TilemapManager.instance.Entities.SetTile(TilemapManager.instance.Entities.WorldToCell(transform.position), TilemapManager.instance.PresentTile);
     }
